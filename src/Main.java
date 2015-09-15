479ec09c75fb8496790bcf421576aadafc7ac6a1
Harshdesa <Harshdesa@users.noreply.github.com>
2015-09-15 12:30:31 -0400

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+	
+	
+	public Main()
+	{
+		Interface i = new Interface();
+		Database.readFromDataset();
+		Database.readFromHeuristic();
+		//mainFunc();
+		
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args)
+	{
+		Main project = new Main();
+		
+	}
+	
+
+}
